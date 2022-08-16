ae4a8aa958ac35106d9a9d18f634389301b26704
voidnowhere <alivnw@outlook.com>
2022-08-16 22:36:08 +0100

    residence category fixed a bug & order by order_by is_active can't be set fixed by correcting the input name

--- resources/views/admin/residence_categories/create.blade.php
+++ resources/views/admin/residence_categories/create.blade.php
@@ -5,7 +5,7 @@
         <x-form.layout>
             <x-form.input_text name="name" type="text" label="Name"/>
             <x-form.input_text name="order" type="text" label="Order"/>
-            <x-form.input_check name="is_active" type="checkbox" :required="false" label="Active"/>
+            <x-form.input_check name="active" type="checkbox" :required="false" label="Active"/>
             <x-form.submit>Create</x-form.submit>
         </x-form.layout>
     </x-form.container>
