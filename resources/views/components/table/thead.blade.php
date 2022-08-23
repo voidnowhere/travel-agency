11d3cc45d261016ce0769763ec6b9a0156645b0a
voidnowhere <alivnw@outlook.com>
2022-08-23 21:31:32 +0100

    Nav & Order index responsive & OrderService date fixe

--- resources/views/components/table/thead.blade.php
+++ resources/views/components/table/thead.blade.php
@@ -3,7 +3,7 @@
 @foreach($columns as $column)
     <th class="py-3 px-6">{{ $column }}</th>
 @endforeach
-<th class="flex justify-center py-3 px-6">
+<th class="flex justify-center py-3 pr-3">
     <x-svg.crud.add
         class="w-6 h-6 hover:cursor-pointer"
         on-click="showCUIframe('{{ $iframeCUId }}', '{{ $routeCreate }}')"/>
