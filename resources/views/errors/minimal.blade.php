c8a922a34fab6cc6b376f42f2c8bc542ad91c94e
voidnowhere <alivnw@outlook.com>
2022-09-01 23:39:41 +0100

    customized colors for laravel-errors

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -14,17 +14,26 @@
             body {
                 font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
             }
+            .bg-color{
+                background-color: #dbeafe;
+            }
+            .text-color{
+                color: #3b82f6;
+            }
+            .border-b-bg{
+                border-right: 1px solid #3b82f6;
+            }
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-color sm:items-center sm:pt-0">
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
-                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                    <div class="px-4 text-lg text-color border-b-bg tracking-wider">
                         @yield('code')
                     </div>
 
-                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                    <div class="ml-4 text-lg text-color uppercase tracking-wider">
                         @yield('message')
                     </div>
                 </div>
