b9c6ceb2b376a8443967821cd169f26327d3795a
voidnowhere <alivnw@outlook.com>
2022-09-07 19:34:15 +0100

    login changed position of recaptcha & reset password

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -4,12 +4,12 @@
             @csrf
             <x-form.input_text name="email" type="email" label="Email"/>
             <x-form.input_text name="password" type="password" label="Password"/>
-            <x-form.recaptcha/>
             <div class="text-center mt-2">
                 <a href="{{ route('password.request') }}" class="underline text-gray-600 hover:text-gray-900">
                     Forgot your password?
                 </a>
             </div>
+            <x-form.recaptcha/>
             <x-form.submit>Login</x-form.submit>
         </form>
     </div>
