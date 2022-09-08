e6d708fe68dd24f7948da3c25dc11681d6b87991
voidnowhere <alivnw@outlook.com>
2022-09-08 12:54:27 +0100

    Switched recaptcha mode to invisible

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,12 +4,11 @@
             <p>Forgot your password? No problem. Just let us know your email address and we will email you a password
                 reset link that will allow you to choose a new one.</p>
         </div>
-        <form method="POST" action="{{ route('password.email') }}"
+        <form method="POST" id="forgot_password_form" action="{{ route('password.email') }}"
               class="px-5 pt-5 pb-2 shadow-lg rounded-lg bg-blue-100 flex flex-col">
             @csrf
             <x-form.input_text name="email" label="Email" type="email"/>
-            <x-form.recaptcha/>
-            <x-form.submit>Email Password Reset Link</x-form.submit>
+            <x-form.recaptcha_invisible label="Email Password Reset Link" form-id="forgot_password_form"/>
         </form>
     </div>
 </x-home.layout>
--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -1,6 +1,6 @@
 <x-home.layout>
     <div class="flex justify-center px-3 sm:px-0">
-        <form method="post" class="mt-20 px-5 pt-5 pb-2 shadow-lg rounded-lg bg-blue-100">
+        <form method="post" id="login_form" class="mt-20 px-5 pt-5 pb-2 shadow-lg rounded-lg bg-blue-100">
             @csrf
             <x-form.input_text name="email" type="email" label="Email"/>
             <x-form.input_text name="password" type="password" label="Password"/>
@@ -9,8 +9,7 @@
                     Forgot your password?
                 </a>
             </div>
-            <x-form.recaptcha/>
-            <x-form.submit>Login</x-form.submit>
+            <x-form.recaptcha_invisible label="Login" form-id="login_form"/>
         </form>
     </div>
 </x-home.layout>
