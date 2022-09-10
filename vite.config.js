2bd9718977f27d228a61f3d554ae63be257ea756
voidnowhere <alivnw@outlook.com>
2022-09-11 00:02:40 +0100

    forgot to config vite new resources paths

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,13 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/jquery-ui.css',
                 // 'resources/js/app.js',
                 'resources/js/all.js',
+                'resources/js/alpinejs.js',
+                'resources/js/notiflix.js',
+                'resources/js/jquery.js',
+                'resources/js/jquery-ui.js',
             ],
             refresh: true,
         }),
