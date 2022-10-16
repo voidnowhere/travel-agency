6251df23b14ae0530997a4c0f81e10a93b067615
voidnowhere <alivnw@outlook.com>
2022-10-16 09:09:29 +0100

    mad price details responsive in home page

--- resources/views/home/orders/layout.blade.php
+++ resources/views/home/orders/layout.blade.php
@@ -5,6 +5,6 @@
         <x-admin.iframe.cu.layout :id="\App\Iframes\UserOrderIframe::$iframeCUId"
                                   width-class="w-full md:w-2/3" height-class="h-full md:h-3/4"/>
         <iframe id="{{ \App\Iframes\OrderIframe::$priceDetailIframeId }}"
-                class="absolute w-5/12 h-2/4 hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></iframe>
+                class="absolute w-11/12 md:w-5/12 h-2/3 md:h-2/4 hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></iframe>
     </div>
 </x-home.layout>
