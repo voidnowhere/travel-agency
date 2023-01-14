6e38f8b5b3401bc5a5da00d085fc1f433587dab9
voidnowhere <alivnw@outlook.com>
2023-01-14 23:15:58 +0100

    Autocomplete should start from 3 chars & Web page title changed

--- resources/views/components/admin/layout/base.blade.php
+++ resources/views/components/admin/layout/base.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cynab Trips</title>
+    <title>Cynab Travels</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/app.css', 'resources/js/all.js'])
     @if($loadJqueryUI)
