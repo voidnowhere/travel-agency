ccb51e1d8bafd9b3da40d51a9c3721ddfd1a7ab8
voidnowhere <alivnw@outlook.com>
2023-06-01 13:40:40 +0100

    Changed naming

--- resources/views/components/home/layout.blade.php
+++ resources/views/components/home/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cynab Travels</title>
+    <title>Travel Agency</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @vite(['resources/css/app.css', 'resources/js/all.js'])
 </head>
